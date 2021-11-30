4161dddff1bc3fb93f8cc8c9a4f95621aa8d7098
praveen2miller <91452751+praveen2miller@users.noreply.github.com>
2021-11-30 19:30:53 +0530

    Add files via upload

---
+++ seq-sql/models/books.js
@@ -0,0 +1,35 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Books extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Books.init({
+    BookId: {
+      type:DataTypes.INTEGER,
+      autoIncrement:true,
+      primaryKey:true,
+          },
+    BookName:{ 
+      type: DataTypes.STRING,
+      allowNull:false
+    },
+        
+  }, {
+    sequelize,
+    modelName: 'Books',
+  });
+  // Books.associate = function(models){
+  //   Books.belongsTo(models.Authers,{foreignKey:'AutherId', as:'Authers'})
+  // }
+  return Books;
+};
